fe46bc84336ef4e610c2c2d1b049ab4c2e012478
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2021-09-15 22:33:50 -0400

    Creates componest for Todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from "./logo.svg";
-import "./App.css";
+// import "./App.css";
+import React from "react";
+import { TodoCounter } from "./TodoCounter";
+import { TodoSearch } from "./TodoSearch";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoList } from "./TodoList";
+import { TodoItem } from "./TodoItem";
+
+const todos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Cortar cebolla 1", completed: false },
+  { text: "Cortar cebolla 2", completed: false },
+  { text: "Descansar", completed: false },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
