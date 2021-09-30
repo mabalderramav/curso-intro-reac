42e759064b02e6378568fa85d3731f1d654b8ee2
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2021-09-29 22:56:43 -0400

    handle events

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,17 +2,28 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
+  const onComplete = () => {
+    alert(`ya completaste el todo ${props.text}`);
+  };
+
+  const onDelete = () => {
+    alert(`borraste el todo ${props.text}`);
+  };
+
   return (
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">X</span>
+      <span className="Icon Icon-delete" onClick={onDelete}>
+        X
+      </span>
     </li>
   );
 }
