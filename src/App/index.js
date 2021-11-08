0679f8d33e23e79b1b7f003b6f83eadb3c801d5b
Miguel Aldo Balderrama Vaca <miguel.aldobv@gmail.com>
2021-11-08 18:57:15 -0500

    re-organize component

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,6 @@
-// import "./App.css";
 import React from "react";
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch";
-import { CreateTodoButton } from "./CreateTodoButton";
-import { TodoList } from "./TodoList";
-import { TodoItem } from "./TodoItem";
+
+import { AppUI } from "./AppUI";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: false },
@@ -50,24 +46,15 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <TodoCounter total={totalTodos} completed={completedTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-
-      <TodoList>
-        {searchedTodos.map((todo) => (
-          <TodoItem
-            key={todo.text}
-            text={todo.text}
-            completed={todo.completed}
-            onComplete={() => completeTodo(todo.text)}
-            onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-    </React.Fragment>
+    <AppUI
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
